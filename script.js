88f6a5735ec476bbc06ee88670bad5129ca84043
Aliya Gamez <aliya.e.gamez@gmail.com>
2025-04-08 14:57:16 -0400

    extra styling

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// Create sketchContainer and styling
+// Create sketchContainer
+const heading = document.createElement("h1");
+heading.textContent = "etch-a-sketch";
+
 const sketchContainer = document.createElement("div");
-const styles = document.createElement("style");
-sketchContainer.classList.add("sketchContainer");
+sketchContainer.classList.add("sketch-container");
+
+const promptContainer = document.createElement("div");
+promptContainer.classList.add("prompt-container");
+
+
 
 // Create 16x16 square divs
 for(let i = 1; i <= 16; i++) {
@@ -15,35 +22,68 @@ for(let i = 1; i <= 16; i++) {
 
 
 // Styling for Elements
+const styles = document.createElement("style");
+var containerWidth = 400;
+var rowCount = 16;
+
 styles.textContent = `
     body {
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
+        
+        background-color: #161C14;
     }
 
-    .sketchContainer {
+    h1 {
+        color: rgb(238, 238, 238);
+    }
+
+    .sketch-container {
         display: flex;
+        justify-content: space-between;
+        align-items: space-between;
         flex-wrap: wrap; 
-        width: 400px;
-        height: 400px;
-        border: 1px solid black;   
+
+        width: ${containerWidth+rowCount-1}px;
+        height: auto;
+
+        row-gap: 1px;
+
+        background-color: rgb(165, 165, 165);
+        border: 4px solid rgb(165, 165, 165);   
     }
 
     .squareDiv {
-        height: 25pxpx;
-        flex: 1 0 25px;
+        height: ${containerWidth/rowCount}px;
+        flex: 0 0 ${containerWidth/rowCount}px;
+
+
+        background-color: #fff;
     }
 
     .squareDiv:hover {
        background: red; 
+    }
+
+    .prompt-container {
+        width: ${containerWidth+rowCount-1}px;
+        height: 50px;
+        margin-top: 20px;
+
+        background: #fff;
+        border: 4px solid rgb(165, 165, 165); 
+        border-radius: 5px;
     }
 `;
 
 
 
 // Place divs on page
+document.body.appendChild(heading);
 document.body.appendChild(sketchContainer);
+document.body.appendChild(promptContainer);
 document.head.appendChild(styles);
 
 for(let i = 1; i <= 16; i++) {
